d9df721a70bad96a654b47f93dd73772b623e2a3
Rodrigo Lima <rodrigolimasd@gmail.com>
2018-03-07 10:11:05 -0300

    Implementação de inclusão de lançamento, configurado joda time

---
+++ src/main/java/com/rl/rmoney/api/repository/LancamentoRepository.java
@@ -0,0 +1,15 @@
+package com.rl.rmoney.api.repository;
+
+import com.rl.rmoney.api.model.Lancamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Rodrigo Lima
+ * @version 1.0
+ * @since 07/03/2018
+ */
+@Repository
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+}
