daa80e1521747aedcf55f17c435c9eb1c9a7152c
Rodrigo Lima <rodrigolimasd@gmail.com>
2018-03-08 20:08:47 -0300

    implementação de token com jwt(json web token) integrado ao oauth2

--- src/main/java/com/rl/rmoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/rl/rmoney/api/config/ResourceServerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
