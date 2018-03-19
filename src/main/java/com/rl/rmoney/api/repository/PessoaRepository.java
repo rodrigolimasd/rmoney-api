80fda8ce764d38b4f149e4bb3bdc254e1a2befe5
Rodrigo Lima <rodrigolimasd@gmail.com>
2018-03-19 11:24:20 -0300

    implementação de pesquisa de pessoa

--- src/main/java/com/rl/rmoney/api/repository/PessoaRepository.java
+++ src/main/java/com/rl/rmoney/api/repository/PessoaRepository.java
@@ -1,6 +1,8 @@
 package com.rl.rmoney.api.repository;
 
 import com.rl.rmoney.api.model.Pessoa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PessoaRepository  extends JpaRepository<Pessoa, Long> {
+
+    public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 
 }
--- src/main/java/com/rl/rmoney/api/resource/PessoaResource.java
+++ src/main/java/com/rl/rmoney/api/resource/PessoaResource.java
@@ -11,6 +11,8 @@ import com.rl.rmoney.api.repository.PessoaRepository;
 import com.rl.rmoney.api.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -34,11 +36,6 @@ public class PessoaResource {
 
     @Autowired
     private PessoaService pessoaService;
-
-    @GetMapping
-    public List<Pessoa> listar() {
-        return pessoaRepository.findAll();
-    }
 
     @PostMapping
     @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
@@ -75,6 +72,12 @@ public class PessoaResource {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
         pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+    public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "%") String nome, Pageable pageable) {
+        return pessoaRepository.findByNomeContaining(nome, pageable);
     }
 
 }
