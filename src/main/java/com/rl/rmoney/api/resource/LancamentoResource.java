24de70af2668699cc70d29a87151fab2b935f517
Rodrigo Lima <rodrigolimasd@gmail.com>
2018-03-19 11:51:15 -0300

    implementação de atualização de lançamentos

--- src/main/java/com/rl/rmoney/api/resource/LancamentoResource.java
+++ src/main/java/com/rl/rmoney/api/resource/LancamentoResource.java
@@ -90,4 +90,15 @@ public class LancamentoResource {
         lancamentoRepository.delete(codigo);
     }
 
+    @PutMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO')")
+    public ResponseEntity<Lancamento> atualizar(@PathVariable Long codigo, @Valid @RequestBody Lancamento lancamento) {
+        try {
+            Lancamento lancamentoSalvo = lancamentoService.atualizar(codigo, lancamento);
+            return ResponseEntity.ok(lancamentoSalvo);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
--- src/main/java/com/rl/rmoney/api/service/LancamentoService.java
+++ src/main/java/com/rl/rmoney/api/service/LancamentoService.java
@@ -5,6 +5,7 @@ import com.rl.rmoney.api.model.Pessoa;
 import com.rl.rmoney.api.repository.LancamentoRepository;
 import com.rl.rmoney.api.repository.PessoaRepository;
 import com.rl.rmoney.api.service.exception.PessoaInexistenteOuInativaException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +33,36 @@ public class LancamentoService {
         lancamentoRepository.save(lancamento);
 
         return lancamento;
+    }
+
+    public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+        Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+        if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+            validarPessoa(lancamento);
+        }
+
+        BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+        return lancamentoRepository.save(lancamentoSalvo);
+    }
+
+    private Lancamento buscarLancamentoExistente(Long codigo) {
+        Lancamento lancamentoSalvo = lancamentoRepository.findOne(codigo);
+        if (lancamentoSalvo == null) {
+            throw new IllegalArgumentException();
+        }
+        return lancamentoSalvo;
+    }
+
+    private void validarPessoa(Lancamento lancamento) {
+        Pessoa pessoa = null;
+        if (lancamento.getPessoa().getCodigo() != null) {
+            pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+        }
+
+        if (pessoa == null || pessoa.isInativo()) {
+            throw new PessoaInexistenteOuInativaException();
+        }
     }
 
 }
